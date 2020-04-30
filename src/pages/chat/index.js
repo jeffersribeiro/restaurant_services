7556d5adb8d4531733361ebdad552ed271de1bc0
Jefferson <jefferson_ribeiro12345@hotmail.com>
2020-04-30 14:54:05 -0300

    chat changes

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -4,27 +4,55 @@ import { Container, Button, Row, Form } from "react-bootstrap";
 import { database, auth } from "../../services/firebase";
 
 class Chat extends Component {
-  state = {
-    message: "",
-    messages: [],
-  };
-  sendMessage(e) {
-    e.preventDefault();
-    var userId = auth.currentUser.uid;
-    database.ref(`chats/chat1/${userId}`).set({
-      message: "message",
+  constructor() {
+    super();
+    this.state = {
+      message: "",
+      data: [],
+      counter: 0,
+    };
+  }
+  sendMessage = (e) => {
+    e.preventDefault('chats/chat1');
+    let { message,counter } = this.state;
+    let userId = auth.currentUser.uid;
+    database.ref(`chats/chat1/${counter}`).set({
+      userId: userId,
+      msg: message,
+      date: `${new Date()}`,
     });
+    this.setState({
+      message: "",
+      counter: this.state.counter + 1,
+    });
+  };
+
+  componentDidMount() {
+    database
+      .ref(`chats/chat1`)
+      .once("value")
+      .then((res) => {
+        this.setState({
+          data: res.val(),
+        });
+      });
   }
 
-  teste() {
-    var userId = auth.currentUser.uid;
+  componentDidUpdate() {
     database
-      .ref(`chats/chat1/${userId}`)
+      .ref(`chats/chat1`)
       .once("value")
-      .then((snapshot) => {
-        var username =
-          (snapshot.val() && snapshot.val().username) || "Anonymous";
-        console.log(username);
+      .then((res) => {
+       console.log(res.val())
+      });
+
+    database
+      .ref(`chats/chat1`)
+      .once("value")
+      .then((res) => {
+        this.setState({
+          data: res.val(),
+        });
       });
   }
 
@@ -37,11 +65,18 @@ class Chat extends Component {
   render() {
     return (
       <Container style={{ height: "100vh" }} fluid>
-        <Row style={{ height: "80vh" }}></Row>
+        <Row style={{ height: "80vh" }}>
+          <ul>
+            {this.state.data.map((e) => (
+              <li>{e.msg}</li>
+            ))}
+          </ul>
+        </Row>
         <Row className="justify-content-md-center">
           <Form onSubmit={this.sendMessage}>
             <Form.Group controlId="formBasicPassword">
               <Form.Control
+                value={this.state.message}
                 onChange={(e) => this.handleMessage(e.target.value)}
                 placeholder="Message.."
               />
@@ -51,7 +86,6 @@ class Chat extends Component {
             </Button>
           </Form>
         </Row>
-        {this.teste()}
       </Container>
     );
   }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,20 +4,21 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 
-// Your web app's Firebase configuration
-let firebaseConfig = {
-  apiKey: "AIzaSyApVj8YgQWb72bCpGdxL4oteaIdQTdaiS8",
-  authDomain: "fir-services-205d1.firebaseapp.com",
-  databaseURL: "https://fir-services-205d1.firebaseio.com",
-  projectId: "fir-services-205d1",
-  storageBucket: "fir-services-205d1.appspot.com",
-  messagingSenderId: "889168483136",
-  appId: "1:889168483136:web:92c5f9e6d6037a12e0e0a5",
-  measurementId: "G-0LWSK6MJ1L",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+function firebaseConfig() {
+  return {
+    apiKey: "AIzaSyApVj8YgQWb72bCpGdxL4oteaIdQTdaiS8",
+    authDomain: "fir-services-205d1.firebaseapp.com",
+    databaseURL: "https://fir-services-205d1.firebaseio.com",
+    projectId: "fir-services-205d1",
+    storageBucket: "fir-services-205d1.appspot.com",
+    messagingSenderId: "889168483136",
+    appId: "1:889168483136:web:92c5f9e6d6037a12e0e0a5",
+    measurementId: "G-0LWSK6MJ1L",
+  };
+}
+
+firebase.initializeApp(firebaseConfig());
 
 export const auth = firebase.auth();
-export const firesore = firebase.firestore();
+export const firestore = firebase.firestore();
 export const database = firebase.database();
