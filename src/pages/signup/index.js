3241a6276f9194c87dc3f34f91ad328b071d9726
Jefferson <jefferson_ribeiro12345@hotmail.com>
2020-06-27 18:43:02 -0300

    finished signup screen

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,30 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import { auth } from "../../services/firebase";
 import { Form, Container } from "./styles";
+import api from "../../services/api";
 
 class SignUp extends Component {
   state = {
-    username:"",
+    username: "",
     email: "",
     password: "",
-    error: ""
+    error: "",
   };
 
-  handleSignIn = async e => {
+  handleSignIn = async (e) => {
     e.preventDefault();
-    const { email, password } = this.state;
-    if (!email || !password) {
+    const {username, email, password } = this.state;
+    if (!username || !email || !password) {
       this.setState({ error: "Preencha e-mail e senha para continuar!" });
     } else {
       try {
-        auth.createUserWithEmailAndPassword(email, password)
+        await api.post("/users",{
+          username, email, password
+        });
         this.props.history.push("/");
       } catch (err) {
         this.setState({
           error:
-            "Houve um problema com o login, verifique suas credenciais. T.T"
+            "Houve um problema com o login, verifique suas credenciais. T.T",
         });
       }
     }
@@ -34,22 +37,25 @@ class SignUp extends Component {
     return (
       <Container>
         <Form onSubmit={this.handleSignIn}>
-          <img src={require('../../assets/images/logo.jpeg')} alt="Airbnb logo" />
+          <img
+            src={require("../../assets/images/logo.jpeg")}
+            alt="Airbnb logo"
+          />
           {this.state.error && <p>{this.state.error}</p>}
           <input
             type="name"
             placeholder="Nome de Usuario"
-            onChange={e => this.setState({ username: e.target.value })}
+            onChange={(e) => this.setState({ username: e.target.value })}
           />
           <input
             type="email"
             placeholder="Endereço de e-mail"
-            onChange={e => this.setState({ email: e.target.value })}
+            onChange={(e) => this.setState({ email: e.target.value })}
           />
           <input
             type="password"
             placeholder="Senha"
-            onChange={e => this.setState({ password: e.target.value })}
+            onChange={(e) => this.setState({ password: e.target.value })}
           />
           <button type="submit">Registrar</button>
         </Form>
