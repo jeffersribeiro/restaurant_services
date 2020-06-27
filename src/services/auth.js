a51e9a74098e161c87eddc9291b543b320241014
Jefferson <jefferson_ribeiro12345@hotmail.com>
2020-06-27 18:41:19 -0300

    taking the token in the api.js file and validating with auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 export const TOKEN_KEY = "";
+export const TOKEN_KEY2 = "@airbnb-Token";
 
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const login = (token) => {
   localStorage.setItem(TOKEN_KEY, token);
