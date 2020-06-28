484e189c3178eb2b14965b34c95de11a604c1cfb
Jefferson <jefferson_ribeiro12345@hotmail.com>
2020-06-27 21:15:04 -0300

    removes firebase link

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
-import { auth } from "../../services/firebase";
 import { Form, Container } from "./styles";
 import api from "../../services/api";
 
